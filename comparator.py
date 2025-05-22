25210077fe07de1e83eb473527ed52513d97b2cc
Eli <134539241+Elinteger@users.noreply.github.com>
2025-05-22 08:54:58 +0200

    todos

--- comparator.py
+++ comparator.py
@@ -13,16 +13,15 @@ def main():
     update_eu = True
     if update_eu:
         update_eu_data()
+    # get chipragdata from prompt
+    # chiprag.main(prompt)
+    # get all distinct pesticides
+    # see (somehow) if pesticide exists in eu data (fetch all pesticides for that, then either fuzzy or LLM) -> dataframe that keeps track of chinese and possible eu
+    # get data for all possible pesticides, ask LLM if there is a value for row from chinese in any of the EU  
+    # get everything into one big dataframe -> thats a different "result dataframe"
+    # dataframe to excel
     print("done")
 
 
 if __name__ == "__main__":
     main()
-
-# plan for SQL
-# saver for eu data 
-# comparison
-    # fuzzy search (vielleicht auf ganze liste an llm?)
-    # llm frage (wenn nicht ganze liste)
-    # pestizide rausholen
-    # für jede reihe querien ob was passendes dabei ist
